97b8b79e93c023c9d33bf6cebe7c7d806db42ff3
Everton de Matos <everton@ssrc.tii.ae>
2024-09-30 16:55:24 +0400

    Adding all training folder to setup.py
    Signed-off-by: Everton de Matos <everton@ssrc.tii.ae>

--- setup.py
+++ setup.py
@@ -25,12 +25,31 @@ setup(
         "Programming Language :: Python :: 3",
     ],
     python_requires='>=3.11',
-    include_package_data=True, 
+    include_package_data=True,
     package_data={
         "ldpi.training": [
             "output/ResCNN/*.pth",
             "output/ResCNN/plots/*.pdf",
         ],
     },
+    data_files=[
+        ("ldpi/training", [
+            "ldpi/training/data.py",
+            "ldpi/training/losses.py",
+            "ldpi/training/model.py",
+            "ldpi/training/plots.py",
+            "ldpi/training/preprocessing.py",
+            "ldpi/training/training.py",
+            "ldpi/training/transforms.py",
+            "ldpi/training/output/ResCNN/best_model_with_center.pth",
+            "ldpi/training/output/ResCNN/pretrained_model.pth",
+            "ldpi/training/output/ResCNN/scripted_quantized_model.pth",
+            "ldpi/training/output/ResCNN/traced_model.pth",
+            "ldpi/training/output/ResCNN/plots/hundred_one.pdf",
+            "ldpi/training/output/ResCNN/plots/max.pdf",
+            "ldpi/training/output/ResCNN/plots/near_max.pdf",
+            "ldpi/training/output/ResCNN/plots/ninety_nine.pdf"
+        ])
+    ],
     zip_safe=False,
 )
